741d82da72dd12a4883a36d44ef72d66d4d0fd31
RustleofCicada <39823912+RustleofCicada@users.noreply.github.com>
2020-01-21 19:01:45 +0100

    Pyboard Code Template

---
+++ Template_PYBV11/Code/run.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+#include "run.h"
+#include "fatfs.h"
+#include "usb_device.h"
+#include "stm32f4xx_hal.h"
+
+static int counte = 0;
+static char buf[20];
+
+void setup()
+{
+	sprintf("Hello world!", 0);
+	LED_animation();
+}
+
+void loop()
+{
+	HAL_GPIO_TogglePin(LED_BLU_GPIO_Port, LED_BLU_Pin);
+	HAL_Delay(100);
+	sprintf((char*)buf, "Counter: %d", counte);
+}
+
+void LED_animation()
+{
+	HAL_Delay(50);
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
+	HAL_Delay(50);
+	HAL_GPIO_WritePin(LED_YEL_GPIO_Port, LED_YEL_Pin, GPIO_PIN_SET);
+	HAL_Delay(50);
+	HAL_GPIO_WritePin(LED_GRN_GPIO_Port, LED_GRN_Pin, GPIO_PIN_SET);
+	HAL_Delay(50);
+	HAL_GPIO_WritePin(LED_BLU_GPIO_Port, LED_BLU_Pin, GPIO_PIN_SET);
+
+	HAL_Delay(50);
+	HAL_GPIO_WritePin(LED_BLU_GPIO_Port, LED_BLU_Pin, GPIO_PIN_SET);
+	HAL_Delay(50);
+	HAL_GPIO_WritePin(LED_GRN_GPIO_Port, LED_GRN_Pin, GPIO_PIN_SET);
+	HAL_Delay(50);
+	HAL_GPIO_WritePin(LED_YEL_GPIO_Port, LED_YEL_Pin, GPIO_PIN_SET);
+	HAL_Delay(50);
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
+}
