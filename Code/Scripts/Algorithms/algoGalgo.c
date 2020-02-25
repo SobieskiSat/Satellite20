486c85f801080530ba8e7574fcc6a7e5ddd6c7eb
TheRootOf <aceszablewski@gmail.com>
2020-02-25 23:41:22 +0100

    PID init commit

--- Code/Scripts/Algorithms/algoGalgo.c
+++ Code/Scripts/Algorithms/algoGalgo.c
@@ -1,22 +1,79 @@
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-//		Algorithm with Proportional feedback
+//		Algorithm with PID feedback
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 #include "main.h"
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx_hal_gpio.h"
 #include <stdbool.h>
-
+#include <stdio.h>
+#include <math.h>
 #include "motors.h"
 
-static void algoGalgo(float yaw)
+float yaw_last_error;
+
+static void algoGalgo(float yaw, float target_yaw)
 {
 	float maxPower = 0.8;	// scale of motor power
+	float TurboMode= 50.0;
 	// Aim center: yaw = 180*
+//	float target_yaw = 180.0;
+	float error=target_yaw-yaw;
+	float thrust = 180.0+TurboMode;
 
-	setMotors(yaw * maxPower * (1.0 / 360.0), (360.0 - yaw) * maxPower * (1.0 / 360.0));
+	if (error<-180.0)
+		error = target_yaw-yaw+360.0;
+	else if (error>180.0)
+		error = target_yaw-yaw-360.0;
+
+
+
+//	print("target_yaw: "); print_float(target_yaw); println("");
+//	print("yaw: "); print_float(yaw); println("");
+
+//	print("error: "); print_float(error); println("");
+	//print("yaw_last_error: "); print_float(yaw_last_error); println("");
+
+	float kp=1.0; //dobrany
+//	float ki=0.5;
+	float kd=500.0; //dobrany 500 ok - 100ms
+
+	float pid_p, pid_i, pid_d;
+
+	pid_p=kp*error;
+	pid_d=kd*((error-yaw_last_error)/10);
+//	pid_i=pid_i+(ki*error);
+
+
+
+//	print("pid_p: "); print_float(pid_p); println("");
+//	print("pid_d: "); print_float(pid_d); println("");
+//	print("pid_i: "); print_float(pid_i); println("");
+
+	float PID;
+//	if (error>-15 && error <15)
+//		PID=pid_p+pid_d+pid_i;
+//	else
+		PID=pid_p+pid_d;
+
+	if (PID>180.0-TurboMode)
+			PID=180.0-TurboMode;
+		else if (PID<-180.0+TurboMode)
+			PID=-180.0+TurboMode;
+
+//	print("PID: "); print_float(PID); println("");
+
+
+	setMotors((thrust - PID) * maxPower * (1.0 / 360.0), (thrust + PID) * maxPower * (1.0 / 360.0) * 0.92);
+//	setMotors(maxPower, maxPower*0.9); //prawie skalibrowane
+
+
+
+//	setMotors((thrust - error) * maxPower * (1.0 / 360.0), (thrust + error) * maxPower * (1.0 / 360.0));
 
 	// Shines purple LED if centered
-	if (yaw >= 178.0 && yaw <= 182.0) HAL_GPIO_WritePin(LEDA_GPIO_Port, LEDA_Pin, GPIO_PIN_SET);
+	if (yaw >= target_yaw-2 && yaw <= target_yaw+2) HAL_GPIO_WritePin(LEDA_GPIO_Port, LEDA_Pin, GPIO_PIN_SET);
 	else HAL_GPIO_WritePin(LEDA_GPIO_Port, LEDA_Pin, GPIO_PIN_RESET);
+
+	yaw_last_error=error;
 }
--- Code/run.c
+++ Code/run.c
@@ -9,9 +9,19 @@
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx_hal_i2c.h"
 #include "stm32f4xx_hal_gpio.h"
+#include <math.h>
+#include <stdlib.h>
 
-#include "Peripherials/sdTest.c"
-#include "logger.h"
+#include "motors.h"
+#include "Peripherials/imuTest.c"
+
+#include "Algorithms/algoGalgo.c"
+
+uint32_t lastMotUpdate;
+uint32_t lastMotUpdate2;
+
+float target_yaw;
+time_t t;
 
 static void setup(void)
 {
@@ -22,11 +32,38 @@ static void setup(void)
 	println("Hello world!!");	HAL_Delay(500);
 	HAL_GPIO_WritePin(LEDB_GPIO_Port, LEDB_Pin, GPIO_PIN_RESET);
 
-	if (sdTest_begin()) println("SD success!!!!");
-	log_new();
+	if (imuTest_begin()) println("[IMU] Init successful!");
+
+	enableMotors(); println("[MOT] Motors enabled!");
+	lastMotUpdate = millis();
+	lastMotUpdate2 = millis();
+
+	yaw_last_error=0.0;
+	target_yaw=180.0;
+
+	srand((unsigned) time(&t));
 }
 
 static void loop(void)
 {
-	sdTest_loop();
+	imuTest_getData();		// get data from IMU
+	imuTest_quatUpdate();	// compute data received
+
+
+/*	if (millis() - lastMotUpdate2 >= 2000)	// every 10ms get Euler angles and run motor alogrithm
+	{
+		target_yaw=rand()%360; // losowo
+		//target_yaw=fmod(target_yaw+1.0, 360);
+
+
+		lastMotUpdate2 = millis();
+	}
+*/
+	if (millis() - lastMotUpdate >= 10)	// every 10ms get Euler angles and run motor alogrithm
+	{
+		imuTest_getEuler();
+		algoGalgo(yaw, target_yaw);
+		lastMotUpdate = millis();
+	}
+
 }
